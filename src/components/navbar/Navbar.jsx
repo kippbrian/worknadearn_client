3d261f6d02341e0e1ce9897c68fd5ecff34094f2
brian kipp <devkippbrian@gmail.com>
2023-03-04 12:34:22 +0300

    third commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import newRequest from "../../utils/newRequest";
-import "./Navbar.scss";
+import "./navbar.scss";
 
 function Navbar() {
   const [active, setActive] = useState(false);
