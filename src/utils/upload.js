6742cc40b8071ed4a508eb1054580227dac3ea99
brian kipp <devkippbrian@gmail.com>
2023-03-13 11:33:34 +0300

    fourth commit

--- src/utils/upload.js
+++ src/utils/upload.js
@@ -4,9 +4,10 @@ const upload = async (file) => {
   const data = new FormData();
   data.append("file", file);
   data.append("upload_preset", "workandearn");
+  data.append("cloud_name", "dn8if7wyf");
 
   try {
-    const res = await axios.post(import.meta.env.VITE_UPLOAD_LINK, data);
+    const res = await axios.post('https://api.cloudinary.com/v1_1/dn8if7wyf/image/upload', data);
 
     const { url } = res.data;
     return url;
